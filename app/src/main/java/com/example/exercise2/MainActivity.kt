25c3e7cceabf31a55dd3cb56f5c7fe7af5f74fdb
anreh <anurehka@gmail.com>
2019-11-25 08:56:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.exercise2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(){
+            val height:Double = editHeight.text.toString().toDouble();
+            val weight:Double = editWeight.text.toString().toDouble();
+
+            val bmi:Double = weight / Math.pow(height,2.0);
+
+            if(bmi < 18.5){
+
+                textValue.text = "UnderWeight: %.2f".format(bmi);
+
+                imageView2.setImageResource(R.drawable.under);
+            }else if (bmi >= 18.5 && bmi <=24.9 ){
+
+                imageView2.setImageResource(R.drawable.normal);
+                textValue.text = "Normal: %.2f".format(bmi);
+            }else{
+
+                imageView2.setImageResource(R.drawable.over);
+                textValue.text = "OverWeight: %.2f".format(bmi);
+            }
+        }
+    }
+}
